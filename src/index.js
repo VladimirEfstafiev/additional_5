00b59266f38307eb6f1b16009e91ff2b00e9465e
Alex Brov <shalak10@outlook.com>
2018-02-21 13:07:02 +0300

    result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports =function check(str, brackets) {
+    let stack = [];
+    for (let i in str) {
+        let currentEl = str[i];
+        for (let j in brackets) {
+                if (currentEl == brackets[j][1]) {
+                    if (brackets[j][0] != brackets[j][1]) {
+                    if (stack[stack.length - 1] == brackets[j][0]) {
+                        stack.pop();
+                    } else {
+                        return false;}
+                    }else{
+                        if (stack[stack.length - 1] == brackets[j][0]) {
+                                stack.pop();
+                            }else{
+                                stack.push(currentEl);
+                            }
+                        }
+                } else if (currentEl == brackets[j][0]) {
+                    stack.push(currentEl);
+                }
+            }
+    }
+    if (stack.length == 0) {
+        return true
+    };
+    return false;
 }
